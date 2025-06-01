6f0a64cbe9b4cc6360d29a4a4690061096518e6c
Pedro Silva <Pedro.Silva@prolion.com>
2025-06-01 18:11:34 +0200

    Major changes regarding the about us,the contacts and a start of the shoping cart menu

---
+++ src/app/components/enterprise/enterprise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enterprise',
+  imports: [],
+  templateUrl: './enterprise.component.html',
+  styleUrl: './enterprise.component.css'
+})
+export class EnterpriseComponent {
+
+}

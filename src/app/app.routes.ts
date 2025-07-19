e12e01f723895d5750b508b4f0bb58aef1e69931
Pedro Silva <Pedro.Silva@prolion.com>
2025-07-19 20:05:32 +0200

    keep going

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,18 +13,27 @@ export const routes: Routes = [
   },
   {
     path: 'enterprise',
-    loadComponent: ()=>import('./components/enterprise/enterprise.component').then(m=>m.EnterpriseComponent)
+    loadComponent: ()=>import('./components/enterprise/enterprise.component').then(m=>m.EnterpriseComponent),
+    pathMatch: 'full'
   },
   {
     path: 'contacts',
-    loadComponent: ()=>import('./components/contacts/contacts.component').then(m=>m.ContactsComponent)
+    loadComponent: ()=>import('./components/contacts/contacts.component').then(m=>m.ContactsComponent),
+    pathMatch: 'full'
   },
   {
     path: 'shopping-cart',
-    loadComponent: ()=>import('./components/shopping-cart/shopping-cart.component').then(m=>m.ShoppingCartComponent)
+    loadComponent: ()=>import('./components/shopping-cart/shopping-cart.component').then(m=>m.ShoppingCartComponent),
+    pathMatch: 'full'
   },
   {
     path: 'final-form',
-    loadComponent: ()=>import('./components/final-form/final-form.component').then(m=>m.FinalFormComponent)
+    loadComponent: ()=>import('./components/final-form/final-form.component').then(m=>m.FinalFormComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'product',
+    loadComponent: () => import('./components/product/product.component').then(m=>m.ProductComponent),
+    pathMatch: 'full'
   }
 ];
